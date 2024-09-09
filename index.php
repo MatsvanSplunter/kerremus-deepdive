3d400c342f48e1ffc57d5320a5600440ae220e08
alexander <alexanderzoet@gmail.com>
2024-09-09 16:21:56 +0200

    alex

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
+    <div class>
+        <h1 class="title">game of live</h1>
+        <button class="buttoncontinue">start</button>
+    </div>
 </body>
 </html>
