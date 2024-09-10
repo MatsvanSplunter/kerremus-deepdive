8845f479715f7659fea19bca3a50c33ec345cac2
Johndaboy <145539556+Johndaboy@users.noreply.github.com>
2024-09-10 13:37:05 +0200

    version 0.2

--- game.js
+++ game.js
@@ -1,25 +1,62 @@
 let size;
+let mousedown;
+let DoSimulate = false;
+let simulation;
+let simbutton = document.querySelector("button");
+let ev;
+let firstcel;
+let lastcel;
 
 const bord = [];
 document.querySelectorAll('tr').forEach((tableRow, index) => {
     const cellenInRow = tableRow.querySelectorAll('td');
     bord[index] = cellenInRow;
     size = index;
-})
+});
 
 for (let x = 0; x < size; x += 1) {
     for (let y = 0; y < size; y += 1) {
-        bord[x][y].addEventListener("click", function () {
-            if(bord[x][y].classList == "true") {
-                bord[x][y].classList = "false";
-            } else {
-                bord[x][y].classList = "true"
+        bord[x][y].addEventListener("mouseover", (e) => {
+            addEventListener("mousedown", (e) => {
+                mousedown = true;
+                firstcel = x, y;
+            });
+            addEventListener("mouseup", (e) => {
+                mousedown = false;
+                lastcel = x, y;
+            });
+            if(mousedown && ev.button == 0) {
+                if(bord[x][y].classList == "true") {
+                    bord[x][y].classList = "false";
+                } else {
+                    bord[x][y].classList = "true";
+                }
+            } else if(mousedown && ev.button == 2) {
+                for(let celx = firstcel.x; celx >= lastcel.x; celx += 1) {
+                    for(let cely = firstcel.y; cely >= lastcel.y; cely += 1) {
+                        if(bord[celx][cely].classList == "true") {
+                            bord[celx][cely].classList = "false";
+                        } else {
+                            bord[celx][cely].classList = "true";
+                        }
+                        
+                    }
+                }
+            }
+        });
+        bord[x][y].addEventListener("mousedown", (e) => {
+            ev = e;
+            if(ev.button == 0) {
+                if(bord[x][y].classList == "true") {
+                    bord[x][y].classList = "false";
+                } else {
+                    bord[x][y].classList = "true";
+                }
             }
         });
     }
 }
 
-let simulation;
 function simulate () {
     let celstrue = [];
     let celsfalse = [];
@@ -91,9 +128,29 @@ function checkcel(classList, omringt)
 
 function simulatebutton()
 {
-    if(simulation == true) {
-        clearInterval(simulation);
-    } else {
+    if(simbutton.innerHTML == "Simulate") {
         simulation = setInterval(simulate, 0);
+        DoSimulate = true;
+        simbutton.innerHTML = "Pause";
+    } else {
+        clearInterval(simulation);
+        DoSimulate = false;
+        simbutton.innerHTML = "Simulate";
     }
 }
+
+document.body.onkeydown = function(e) {
+    if (e.key == " ") {
+        simulatebutton();
+    }
+}
+
+window.onkeydown = function(e) { 
+    return !(e.keyCode == 32 && e.target == document.body);
+};
+document.addEventListener('contextmenu', event => {
+    event.preventDefault();
+});
+document.addEventListener('mousedown', event => {
+    event.preventDefault();
+});
