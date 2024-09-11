c6a80df255ea514e903e8de61d5152f5136e97fa
Johndaboy <145539556+Johndaboy@users.noreply.github.com>
2024-09-11 15:50:42 +0200

    .

--- game.php
+++ game.php
@@ -42,7 +42,7 @@ $celsize = 25;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="game.css">
-    <title>Document</title>
+    <title>Game of Life</title>
 </head>
 
 <body>
