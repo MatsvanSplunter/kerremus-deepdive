7f08d2ca3160d2becf869e63780623ecd4f745dd
alexander <alexanderzoet@gmail.com>
2024-09-11 16:13:39 +0200

    alex

--- shop.php
+++ shop.php
@@ -13,26 +13,87 @@
         <div class="cards">
             <h1>red</h1>
             <img src="red.png" alt="red">
+            <p>price: 200</p>
             <button class="neon-btn">buy</button>
         </div>
         <div class="cards">
             <h1>yellow</h1>
             <img src="yellow.png" alt="red">
+            <p>price: 400</p>
             <button class="neon-btn">buy</button>
         </div>
         <div class="cards">
-            <h1>red</h1>
+            <h1>pink</h1>
             <img src="pink.png" alt="red">
+            <p>price: 600</p>
             <button class="neon-btn">buy</button>
         </div>
         <div class="cards">
             <h1>blue</h1>
             <img src="blue.png" alt="red">
+            <p>price: 800</p>
             <button class="neon-btn">buy</button>
         </div>
         <div class="cards">
             <h1>orange</h1>
             <img src="orange.png" alt="red">
+            <p>price: 1k</p>
+            <button class="neon-btn">buy</button>
+        </div>
+    </div>
+    <h1>cell color</h1>
+    <div class="cardrowcolors">
+        <div class="cards">
+            <h1>red</h1>
+            <p>price: 200</p>
+            <button class="neon-btn">buy</button>
+        </div>
+        <div class="cards">
+            <h1>yellow</h1>
+            <p>price: 400</p>
+            <button class="neon-btn">buy</button>
+        </div>
+        <div class="cards">
+            <h1>pink</h1>
+            <p>price: 600</p>
+            <button class="neon-btn">buy</button>
+        </div>
+        <div class="cards">
+            <h1>transparent</h1>
+            <p>price: 2k</p>
+            <button class="neon-btn">buy</button>
+        </div>
+        <div class="cards">
+            <h1>black</h1>
+            <p>price: 10k</p>
+             <button class="neon-btn">buy</button>
+        </div>
+    </div>
+    <h1>background colors</h1>
+    <div class="cardrowcolors">
+        <div class="cards">
+            <h1>red</h1>
+            <p>price: 200</p>
+            <button class="neon-btn">buy</button>
+        </div>
+        <div class="cards">
+            <h1>yellow</h1>
+            <p>price: 400</p>
+            <button class="neon-btn">buy</button>
+        </div>
+        <div class="cards">
+            <h1>pink</h1>
+            <p>price: 600</p>
+            <button class="neon-btn">buy</button>
+        </div>
+        <div class="cards">
+            <h1>blue</h1>
+            <p>price: 800</p>
+            <button class="neon-btn">buy</button>
+        </div>
+        <div class="cards">
+            <h1>orange</h1>
+            <p>price: 1k</p>
             <button class="neon-btn">buy</button>
         </div>
     </div>
