6df747bc58767c8faceeeb1029f71f3888382f59
alexander <alexanderzoet@gmail.com>
2024-09-11 15:05:24 +0200

    alex

--- shop.php
+++ shop.php
@@ -4,9 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/shop.css">
+    <link rel="stylesheet" href="shop.css">
 </head>
 <body>
-    
+    <h1 class="title">Shop</h1>
+    <h1>glowing colors</h1>
+    <div class="cardrowcolors">
+        <div class="cards">
+            <h1>red</h1>
+            <img src="red.png" alt="red">
+            <button class="neon-btn">buy</button>
+        </div>
+        <div class="cards">
+            <h1>yellow</h1>
+            <img src="yellow.png" alt="red">
+            <button class="neon-btn">buy</button>
+        </div>
+        <div class="cards">
+            <h1>red</h1>
+            <img src="pink.png" alt="red">
+            <button class="neon-btn">buy</button>
+        </div>
+        <div class="cards">
+            <h1>blue</h1>
+            <img src="blue.png" alt="red">
+            <button class="neon-btn">buy</button>
+        </div>
+        <div class="cards">
+            <h1>orange</h1>
+            <img src="orange.png" alt="red">
+            <button class="neon-btn">buy</button>
+        </div>
+    </div>
 </body>
 </html>
