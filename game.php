fcd32b7bede8b2de26baef5bc64ab742a7e434af
Johndaboy <145539556+Johndaboy@users.noreply.github.com>
2024-09-11 16:31:20 +0200

    .

--- game.php
+++ game.php
@@ -33,6 +33,16 @@ if ($_POST['gridsize']) {
     }
 }
 
+function parsebool($int) {
+    if($int == 1) {
+        return true;
+    } else if($int == 0) {
+        return false;
+    } else {
+        return "not a bool number";
+    }
+}
+
 $celsize = 25;
 ?>
 <!DOCTYPE html>
@@ -73,8 +83,46 @@ $celsize = 25;
         document.documentElement.style.setProperty('--cell-size', '25px');
     </script>
     <div class="bottom-bar">
-        <h3>no patterns saved</h3>
-    </div>
+        <?php
+        if($patterns != []) {
+            ?>
+            <h3>no patterns saved</h3>
+        </div>
+        <?php
+    } else {
+        ?>
+        <script>
+            let bottombar = document.getElementsByClassName("bottom-bar");
+        </script>
+        <?php
+        foreach($patterns as $pattern) {
+            $patternbord = [];
+            foreach (explode(".", $pattern['pattern']) as $index => $patter){
+                $patternbord[$index] = explode("", $patter);
+            }
+            ?>
+            <div>
+                <script>
+                    const [width, height] = [<?=explode(", ", $pattern['gamesize'])?>];
+                    let patternbord = <?=$patternbord?>;
+                    table = document.createElement('table');
+                    table.classList.add('grid');
+                    tableHTML = '';
+                    for (let row = 0; row < height; row++) {
+                        tableHTML += "<tr>";
+                        for (let col = 0; col < width; col++) {
+                            tableHTML += `<td class='pattern <?=parsebool($patternbord[$row][$col])?>' id='${row},${col}'></td>`;
+                        }
+                        tableHTML += "</tr>";
+                    }
+                    bottombar.appendChild(table);
+                    table.innerHTML = tableHTML;
+                </script>
+            </div>
+            <?php
+        }
+    }
+    ?>
 </body>
 <script src="game.js"></script>
 
