bb6d44d43f01331c7fd34280b999df56b310d869
Johndaboy <145539556+Johndaboy@users.noreply.github.com>
2024-09-12 10:29:08 +0200

    .

--- data.php
+++ data.php
@@ -3,14 +3,6 @@
 //dit is geen pagina dit is een file puur om op te slaan!
 
 include_once("connect.php");
-$dbhost = "localhost";
-$dbname = "kerremus_deepdive";
-$dbuser = "bit_academy";
-$dbpass = "bit_academy";
-
-$pdo = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
 session_start();
 
 if($_POST['function']) {
